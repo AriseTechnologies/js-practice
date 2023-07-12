929b3f6b8b737a8efa278c534c6f5721f9318cb0
Umesh Manikdurge <49201790+AriseTechnologies@users.noreply.github.com>
2023-07-12 17:25:14 +0000

    datatypes commit

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all js code as newer version
+// alert("Hello") //we are using nodeJs not browser
+let name = "Umesh"
+let age = 30
+let isLogin = true
+//Number -
+// bigint
+// String->""
+// boolean ->t/f
+// null -> standalone value
+// undefined 
+// symbol->uniqueness
+
+
+// object
+console.log(typeof "umesh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
