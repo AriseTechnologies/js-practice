844971890759b335a740d317b741a74100877996
Umesh Manikdurge <49201790+AriseTechnologies@users.noreply.github.com>
2023-07-12 05:47:08 +0000

    variables added

---
+++ 02_variables/variable.js
@@ -0,0 +1,6 @@
+const accountId = 12345;
+let accountEmail = "umesh@gmail.com";
+var accountPwd = "umss";
+let accountCity;
+// accountId = 1212; // const can't be changed
+console.table([accountId,accountEmail,accountPwd,accountCity]);
